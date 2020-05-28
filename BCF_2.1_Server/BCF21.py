53eba787adb10c4b720c786b7af9eea2cfb3560b
Thomas Magraff <thomas.magraff@bundebygg.no>
2020-05-28 12:06:06 +0200

    Importert bcf til dict

--- BCF_2.1_Server/BCF21.py
+++ BCF_2.1_Server/BCF21.py
@@ -3,12 +3,16 @@ import os
 import xml.etree.ElementTree as ET
 from pyunpack import Archive
 
-bcffil = r'C:\Users\thomasm\BundeGruppen AS\BIM utvikling - General\99 - Scripting\Python\BCF_Server\test.bcf'
+bcffil = r'C:\Playground\BCF_Server\BCF_2.1_Server\test.bcf'
 temploc= r'C:\temp'
 Archive(bcffil).extractall(temploc)
+
+saker = {}
 for nr,issues in enumerate(os.listdir(temploc)):
+
     if os.path.isdir(temploc+'\\'+issues):
-        print (issues,'nr: '+str(nr+1))
+        saker[issues]={}
+        #print (issues,'nr: '+str(nr+1))
 
         #with open(temploc+'\\'+issues+'\markup.bcf','r') as sak:
         #    print (sak.read())
@@ -19,37 +23,49 @@ for nr,issues in enumerate(os.listdir(temploc)):
 
         # Loops for each tag
         def loop_topics(bcf_root):
+            topic={}
             for idx in range(0, len(bcf)):
                 if bcf[idx].text is None:
                     bcf[idx].text = ""
-                print("    " + bcf[idx].tag + ":  " + bcf[idx].text)
+                topic[bcf[idx].tag]=bcf[idx].text
+            return (topic)
 
 
         def loop_comments(bcf_root):
+            comments={}
             for idx in range(0, len(bcf)):
                 #print (idx)
                 if bcf[idx].text is None:
                     bcf[idx].text = ""
-                print("    " + bcf[idx].tag + ":  " + bcf[idx].text)
+                comments[bcf[idx].tag]=bcf[idx].text
+            return comments
 
 
         def loop_viewpoints(bcf_root):
+            vps={}
             for idx in range(0, 2):
                 if bcf[idx].text is None:
                     bcf[idx].text = ""
-                print("    " + bcf[idx].tag + ":  " + bcf[idx].text)
+                vps[bcf[idx].tag]=bcf[idx].text
+            return vps
+                #print("    " + bcf[idx].tag + ":  " + bcf[idx].text)
 
         idx_C=1
         idx_VP=1
         for bcf in bcf_root:
             if bcf.tag == "Topic":
                 print("___ Topic ___")
-                loop_topics(bcf)
+                saker[issues]['Sak']=(loop_topics(bcf))
+
             elif bcf.tag == "Comment":
-                print("___ Comment: ", idx_C, " ___")
-                loop_comments(bcf)
+                #print("___ Comment: ", idx_C, " ___")
+                saker[issues]['Comments']=loop_comments(bcf)
                 idx_C += 1
             elif bcf.tag == "Viewpoints":
-                print("___ Viewpoint: ", idx_VP, " ___")
-                loop_viewpoints(bcf)
+                #print("___ Viewpoint: ", idx_VP, " ___")
+                saker[issues]['Viewpoints']=loop_viewpoints(bcf)
                 idx_VP += 1
+#print (saker)
+for sak,val in saker.items():
+    print (sak,' - ',val)
+
